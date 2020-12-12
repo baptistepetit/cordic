7bdbc81362957fac6030dddaa8c41614a7d57edc
Baptiste Petit <petit.baptiste21@gmail.com>
2020-12-12 18:01:06 +0900

    feat(poc): add a function to pre-compute the CORDIC gain

--- app.js
+++ app.js
@@ -6,7 +6,17 @@ function generateAngleLUT(size) {
     return lut;
 }
 
+function calculateGainFromAngleLUT(lut) {
+    let gain = 1;
+    lut.forEach(element => {
+        gain = gain * Math.cos(element);
+    });
+    return gain;
+}
+
 const iterations = 20;
 const angleLut = generateAngleLUT(iterations);
+const gain = calculateGainFromAngleLUT(angleLut);
 
 console.log("Generated CORDIC Angle LUT is: ", angleLut);
+console.log("CORDIC Gain is: ", gain);
