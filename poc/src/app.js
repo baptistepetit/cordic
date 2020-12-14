0c5ff0761a8085fe1a1b10ca91afb999c2db76fc
Baptiste Petit <petit.baptiste21@gmail.com>
2020-12-14 11:52:31 +0100

    chore(poc): clean out printfs

--- poc/src/app.js
+++ poc/src/app.js
@@ -14,16 +14,6 @@ export function run() {
 
     const res = calculateCordicCosine(targetAngle, cordicIterations);
 
-    console.log(
-        "Coordinates rotated through CORDIC algorithm: ",
-        res
-    );
-
-    console.log(
-        "Maths functions result: ",
-        {x_n: Math.cos(targetAngle), y_n: Math.sin(targetAngle)}
-    );
-
     renderer.clear();
     renderer.drawUnitCircle();
     renderer.drawVector(Math.cos(targetAngle), Math.sin(targetAngle), "#FF0000", 5);
