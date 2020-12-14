c61ee27b9f1754923122752d83a6cf141f0e0490
Baptiste Petit <petit.baptiste21@gmail.com>
2020-12-14 11:11:10 +0100

    refactor(poc): make the app run in a dedicated function

--- poc/src/app.js
+++ poc/src/app.js
@@ -1,11 +1,16 @@
 import { calculateCordicCosine } from '../src/cordic.js'
 
-console.log(
-    "Coordinates rotated through CORDIC algorithm: ",
-    calculateCordicCosine(Math.PI / 4, 20)
-);
+export function run() {
+    console.log(
+        "Coordinates rotated through CORDIC algorithm: ",
+        calculateCordicCosine(Math.PI / 4, 20)
+    );
 
-console.log(
-    "Maths functions result: ",
-    {x_n: Math.cos(Math.PI / 4), y_n: Math.sin(Math.PI / 4)}
-);
+    console.log(
+        "Maths functions result: ",
+        {x_n: Math.cos(Math.PI / 4), y_n: Math.sin(Math.PI / 4)}
+    );
+
+}
+
+run();
--- poc/webpack.config.cjs
+++ poc/webpack.config.cjs
@@ -5,5 +5,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
+        libraryTarget: 'var',
+        library: 'CordicApp'
     }
 };
