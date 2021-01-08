e20ab50311c6806cb453c286a12a059716b36e26
Baptiste Petit <petit.baptiste21@gmail.com>
2021-01-08 15:30:09 +0100

    feat(hardware_design): add an implementation to the cordic pipeline stage

---
+++ hardware_design/rtl/cordic_stage.vhd
@@ -0,0 +1,95 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.ALL;
+
+entity cordic_stage is
+    generic(
+        angular_data_width : integer := 16;
+        linear_data_width  : integer := 16;
+        stage_index        : integer := 0;
+        stage_phase        : integer := 0
+    );
+    port(
+        i_clk        : in std_logic;
+        i_reset      : in std_logic;
+        i_phase      : in std_logic_vector((angular_data_width-1) downto 0);
+        i_position_x : in std_logic_vector((linear_data_width-1) downto 0);
+        i_position_y : in std_logic_vector((linear_data_width-1) downto 0);
+        o_phase      : out std_logic_vector((angular_data_width-1) downto 0);
+        o_position_x : out std_logic_vector((linear_data_width-1) downto 0);
+        o_position_y : out std_logic_vector((linear_data_width-1) downto 0)
+    );
+end cordic_stage;
+
+
+architecture behavioral of cordic_stage is
+
+component add_sub is
+    generic(
+        data_width : integer := 16
+    );
+    port(
+        i_clk            : in std_logic;
+        i_reset          : in std_logic;
+        i_lhs            : in std_logic_vector((data_width-1) downto 0);
+        i_rhs            : in std_logic_vector((data_width-1) downto 0);
+        i_is_substraction : in std_logic;
+        o_result : out std_logic_vector((data_width-1) downto 0)
+    );
+end component add_sub;
+
+constant c_STAGE_PHASE : std_logic_vector((angular_data_width-1) downto 0) :=
+    std_logic_vector(to_signed(stage_phase, angular_data_width));
+
+signal negative_rotation_needed : std_logic;
+signal shifted_position_x : std_logic_vector((linear_data_width-1) downto 0);
+signal shifted_position_y : std_logic_vector((linear_data_width-1) downto 0);
+
+begin
+    negative_rotation_needed <=
+        '1' when signed(i_phase) >= to_signed(0, angular_data_width) else
+        '0';
+
+    shifted_position_x <= std_logic_vector(
+        shift_right(signed(i_position_x), stage_index)
+    );
+    shifted_position_y <= std_logic_vector(
+        shift_right(signed(i_position_y), stage_index)
+    );
+
+    add_sub_1 : add_sub
+    generic map (
+        data_width => linear_data_width
+    ) port map (
+        i_clk => i_clk,
+        i_reset => i_reset,
+        i_lhs => i_position_x,
+        i_rhs => shifted_position_y,
+        i_is_substraction => negative_rotation_needed,
+        o_result => o_position_x
+    );
+
+    add_sub_2 : add_sub
+    generic map (
+        data_width => linear_data_width
+    ) port map (
+        i_clk => i_clk,
+        i_reset => i_reset,
+        i_lhs => i_position_y,
+        i_rhs => shifted_position_x,
+        i_is_substraction => not(negative_rotation_needed),
+        o_result => o_position_y
+    );
+
+    add_sub_3 : add_sub
+    generic map (
+        data_width => angular_data_width
+    ) port map (
+        i_clk => i_clk,
+        i_reset => i_reset,
+        i_lhs => i_phase,
+        i_rhs => c_STAGE_PHASE,
+        i_is_substraction => negative_rotation_needed,
+        o_result => o_phase
+    );
+end behavioral;
---
+++ hardware_design/test/cordic_stage_tb.vhd
@@ -0,0 +1,160 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.ALL;
+
+library vunit_lib;
+context vunit_lib.vunit_context;
+
+library cordic_lib;
+
+entity cordic_stage_tb is
+    generic(
+        runner_cfg : string
+    );
+end cordic_stage_tb;
+
+
+architecture sim of cordic_stage_tb is
+
+-- Constants
+constant c_CLK_PERIOD         : time := 10 us;
+constant c_RESET_TIME         : time := 104 us;
+constant c_ANGULAR_DATA_WIDTH : integer := 15;
+constant c_LINEAR_DATA_WIDTH  : integer := 15;
+constant c_STAGE_INDEX_0      : integer := 0;
+constant c_STAGE_PHASE_0      : integer := 16#1000#;
+constant c_STAGE_INDEX_1      : integer := 1;
+constant c_STAGE_PHASE_1      : integer := 16#0972#;
+
+-- Inputs
+signal clk          : std_logic := '0';
+signal reset        : std_logic := '0';
+signal i_position_x : std_logic_vector(c_LINEAR_DATA_WIDTH-1 downto 0) := (others => 'U');
+signal i_position_y : std_logic_vector(c_LINEAR_DATA_WIDTH-1 downto 0) := (others => 'U');
+signal i_phase      : std_logic_vector(c_ANGULAR_DATA_WIDTH-1 downto 0) := (others => 'U');
+
+-- Output
+signal result_phase_0      : std_logic_vector(c_ANGULAR_DATA_WIDTH-1 downto 0);
+signal result_position_x_0 : std_logic_vector(c_LINEAR_DATA_WIDTH-1 downto 0);
+signal result_position_y_0 : std_logic_vector(c_LINEAR_DATA_WIDTH-1 downto 0);
+signal result_phase_1      : std_logic_vector(c_ANGULAR_DATA_WIDTH-1 downto 0);
+signal result_position_x_1 : std_logic_vector(c_LINEAR_DATA_WIDTH-1 downto 0);
+signal result_position_y_1 : std_logic_vector(c_LINEAR_DATA_WIDTH-1 downto 0);
+signal expected_phase      : std_logic_vector(c_ANGULAR_DATA_WIDTH-1 downto 0);
+signal expected_position_x : std_logic_vector(c_LINEAR_DATA_WIDTH-1 downto 0);
+signal expected_position_y : std_logic_vector(c_LINEAR_DATA_WIDTH-1 downto 0);
+
+begin
+
+    dut_stage_0 : entity cordic_lib.cordic_stage
+    generic map (
+        angular_data_width => c_ANGULAR_DATA_WIDTH,
+        linear_data_width  => c_LINEAR_DATA_WIDTH,
+        stage_index        => c_STAGE_INDEX_0,
+        stage_phase        => c_STAGE_PHASE_0
+    ) port map (
+        i_clk        => clk,
+        i_reset      => reset,
+        i_phase      => i_phase,
+        i_position_x => i_position_x,
+        i_position_y => i_position_y,
+        o_phase      => result_phase_0,
+        o_position_x => result_position_x_0,
+        o_position_y => result_position_y_0
+    );
+
+    dut_stage_1 : entity cordic_lib.cordic_stage
+    generic map (
+        angular_data_width => c_ANGULAR_DATA_WIDTH,
+        linear_data_width  => c_LINEAR_DATA_WIDTH,
+        stage_index        => c_STAGE_INDEX_1,
+        stage_phase        => c_STAGE_PHASE_1
+    ) port map (
+        i_clk        => clk,
+        i_reset      => reset,
+        i_phase      => i_phase,
+        i_position_x => i_position_x,
+        i_position_y => i_position_y,
+        o_phase      => result_phase_1,
+        o_position_x => result_position_x_1,
+        o_position_y => result_position_y_1
+    );
+
+    p_clk : process
+    begin
+        clk <= '0';
+        wait for c_CLK_PERIOD / 2;
+        clk <= '1';
+        wait for c_CLK_PERIOD / 2;
+    end process p_clk;
+
+    p_main : process
+    begin
+        test_runner_setup(runner, runner_cfg);
+
+        while test_suite loop
+            if run("synchronous_reset_works") then
+                reset <= '1';
+                expected_phase      <= (others => '0');
+                expected_position_x <= (others => '0');
+                expected_position_y <= (others => '0');
+                wait for c_CLK_PERIOD;
+                check_not_unknown(result_phase_0);
+                check_not_unknown(result_position_x_0);
+                check_not_unknown(result_position_y_0);
+                check_equal(result_phase_0,      expected_phase);
+                check_equal(result_position_x_0, expected_position_x);
+                check_equal(result_position_y_0, expected_position_y);
+                wait for c_RESET_TIME;
+                reset <= '0';
+            elsif run("negative_rotation_works_stage_0") then
+                i_phase      <= std_logic_vector(to_unsigned(16#0826#, 15));
+                i_position_x <= std_logic_vector(to_unsigned(16#136E#, 15));
+                i_position_y <= std_logic_vector(to_unsigned(16#0000#, 15));
+                expected_phase      <= std_logic_vector(to_unsigned(16#7826#, 15));
+                expected_position_x <= std_logic_vector(to_unsigned(16#136E#, 15));
+                expected_position_y <= std_logic_vector(to_unsigned(16#136E#, 15));
+                wait for c_CLK_PERIOD;
+                check_equal(result_phase_0,      expected_phase);
+                check_equal(result_position_x_0, expected_position_x);
+                check_equal(result_position_y_0, expected_position_y);
+            elsif run("positive_rotation_works_stage_0") then
+                i_phase      <= std_logic_vector(to_unsigned(16#77DA#, 15));
+                i_position_x <= std_logic_vector(to_unsigned(16#136E#, 15));
+                i_position_y <= std_logic_vector(to_unsigned(16#0000#, 15));
+                expected_phase      <= std_logic_vector(to_unsigned(16#07DA#, 15));
+                expected_position_x <= std_logic_vector(to_unsigned(16#136E#, 15));
+                expected_position_y <= std_logic_vector(to_unsigned(16#6C92#, 15));
+                wait for c_CLK_PERIOD;
+                check_equal(result_phase_0,      expected_phase);
+                check_equal(result_position_x_0, expected_position_x);
+                check_equal(result_position_y_0, expected_position_y);
+            elsif run("negative_rotation_works_stage_1") then
+                i_phase      <= std_logic_vector(to_unsigned(16#07DA#, 15));
+                i_position_x <= std_logic_vector(to_unsigned(16#136E#, 15));
+                i_position_y <= std_logic_vector(to_unsigned(16#6C92#, 15));
+                expected_phase      <= std_logic_vector(to_unsigned(16#7E68#, 15));
+                expected_position_x <= std_logic_vector(to_unsigned(16#1D25#, 15));
+                expected_position_y <= std_logic_vector(to_unsigned(16#7649#, 15));
+                wait for c_CLK_PERIOD;
+                check_equal(result_phase_1,      expected_phase);
+                check_equal(result_position_x_1, expected_position_x);
+                check_equal(result_position_y_1, expected_position_y);
+            elsif run("positive_rotation_works_stage_1") then
+                i_phase      <= std_logic_vector(to_unsigned(16#7826#, 15));
+                i_position_x <= std_logic_vector(to_unsigned(16#136E#, 15));
+                i_position_y <= std_logic_vector(to_unsigned(16#136E#, 15));
+                expected_phase      <= std_logic_vector(to_unsigned(16#0198#, 15));
+                expected_position_x <= std_logic_vector(to_unsigned(16#1D25#, 15));
+                expected_position_y <= std_logic_vector(to_unsigned(16#09B7#, 15));
+                wait for c_CLK_PERIOD;
+                check_equal(result_phase_1,      expected_phase);
+                check_equal(result_position_x_1, expected_position_x);
+                check_equal(result_position_y_1, expected_position_y);
+            end if;
+        end loop;
+
+        test_runner_cleanup(runner);
+    end process p_main;
+
+end sim;
