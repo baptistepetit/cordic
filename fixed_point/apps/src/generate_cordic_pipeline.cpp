038383a4a83d3bf427ddf22d59b5c8bb885445f2
Baptiste Petit <petit.baptiste21@gmail.com>
2021-01-11 15:54:44 +0100

    feat(fixed_point): add command line option to set path of generated code file

--- fixed_point/apps/src/generate_cordic_pipeline.cpp
+++ fixed_point/apps/src/generate_cordic_pipeline.cpp
@@ -5,12 +5,23 @@
 #include "../include/generate_cordic_pipeline.hpp"
 #include "../include/write_to_file.hpp"
 
-int main() {
+int main(int argc, char **argv) {
     constexpr unsigned pipelineSize = 12;
     std::stringstream generatedCode =
         generateCordicPipelineCode<2, 13, 15>(pipelineSize);
 
-    const std::string filePath = "";
+    std::string filePath = "";
+    if (argc > 1) {
+        std::string argv1(argv[1]);
+        if ((argv1 == "-h") || (argv1 == "--help")) {
+            std::cerr << "Usage " << argv[0] << " [-p PATH_TO_FILE]"
+                << std::endl;
+            return 0;
+        } else if ((argv1 == "-p") && (argc == 3)) {
+            filePath = std::string(argv[2]);
+        }
+    }
+
     if (writeToFile(filePath, generatedCode)) {
         std::cout << "Content have been written to: "
             << "\"" << filePath << "\""
