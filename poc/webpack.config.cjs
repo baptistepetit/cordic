756d7726247bb66f1fa38b3d540e3d8bacb61e26
Baptiste Petit <petit.baptiste21@gmail.com>
2021-03-27 22:36:54 +0900

    feat(poc/website): add a favicon to the site

--- poc/webpack.config.cjs
+++ poc/webpack.config.cjs
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -13,6 +14,11 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: 'index.html'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: "assets" },
+            ],
         }),
     ],
 };
